158bf932fb884552ca48bee52daaaf694278d289
solersol <ttpage1tk@gmail.com>
2024-08-12 02:13:40 +0300

    vfd

--- src/sections/StickyFooter.jsx
+++ src/sections/StickyFooter.jsx
@@ -7,13 +7,13 @@ const StickyFooter = () => {
   return (
     <footer className="fixed bottom-0 left-0 right-0  py-4 flex flex-col items-center justify-center z-50">
       <div className="animate-bounce flex justify-center space-x-4 mb-4">
-        <a href="https://x.com/PiePioOnSol">
+        <a href="https://x.com/Mylo_Sol_">
           <img src={img1} alt="Image 1" className="w-16 h-16 sm:w-12 sm:h-12 transition-transform transform hover:scale-125" />
         </a>
         {/* <a href="https://pump.fun/">
           <img src={img2} alt="Image 2" className="w-16 h-16 sm:w-12 sm:h-12 transition-transform transform hover:scale-125" />
         </a> */}
-        <a href="https://t.me/PIEPIOSOL">
+        <a href="https://t.me/Myloshow">
           <img src={img3} alt="Image 3" className="w-16 h-16 sm:w-12 sm:h-12 transition-transform transform hover:scale-125" />
         </a>
       </div>
